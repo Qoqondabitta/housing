c055c60b34a734ec92c01c8498f2d699ca94faee
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-08-09 19:43:51 +0500

    housing started

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 export const Root = () => {
   return <BrowserRouter>
       <Routes>
         <Route path='/home' element={<h1>Home</h1>} />
         <Route path='/properties' element={<h1>Properties</h1>} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+        <Route path='/' element={<Navigate to="/home" />} />
       </Routes>
   </BrowserRouter>
 }
